398c6f1b1844b78446249e286ede69e4cf2af688
JustJunior82 <juniornguyen82@gmail.com>
2023-11-30 12:45:27 -0800

    Added reset button to create new grid custom sides
    created functions to destroy current grid and get input for numSquares

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 let defaultSquares = 16;
 let container = document.querySelector('#container');
+let reset = document.querySelector(`#reset`);
+
+
 function createGrid(numSquares) {
     let squareDimension = (750 / numSquares)-2;
-    console.log(squareDimension);
     for (let i = 0; i < numSquares; i++)
     {
         for (let j = 0; j < numSquares; j++)
@@ -13,9 +15,11 @@ function createGrid(numSquares) {
             square.style.width = `${squareDimension}px`;
             square.style.height = `${squareDimension}px`;
             container.appendChild(square);
-        
+
         }
     }
+    let squareList = document.querySelectorAll(`.square`);
+    colorSquares(squareList);
 }
 
 function colorSquares(list) {
@@ -27,7 +31,24 @@ function colorSquares(list) {
     }
 }
 
+function destroyGrid(list) {
+    for (let i = 0, len = list.length; i < len; i++)
+    {
+        container.removeChild(list[i]);             
+    }
+}
+
+reset.addEventListener("click", () => {
+    let numSquares = prompt("Please enter a number of squares between 1 and 100 for your sketchpad!");
+    if (numSquares < 1 || numSquares > 100)
+        console.log("Error! Please enter a valid number of squares!");
+    else
+    {
+        let squareList = document.querySelectorAll(`.square`);
+        destroyGrid(squareList);
+        createGrid(numSquares);
+    }
+})
 
 createGrid(defaultSquares);
-let squareList = document.querySelectorAll(`.square`);
-colorSquares(squareList);
+
